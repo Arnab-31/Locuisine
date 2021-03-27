c3853d7709b5133b1a35edcaa19127a55d1736bd
Krish Chopra <krish.chopra23@gmail.com>
2021-03-27 13:20:17 -0400

    Added marker functionality on map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,28 +1,68 @@
-import React, { useState } from 'react'
-import { GoogleMap, useLoadScript } from '@react-google-maps/api'
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
-const Map = () => {
-  const [ myMap, setMyMap ] = useState(null);
-  const [ center, setCenter ] = useState({ lat: 29.972065, lng: -90.111533 });
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyCTOD1Und52oU9unc9TRWlR0UOXqCEyFc8',
-  });
+export class MapContainer extends Component {
 
-  const renderMap = () => (
-        <GoogleMap
-          mapContainerStyle={{
-            height: "100%",
-            width: "100%",
-          }}
-          zoom={10}
-          center={center}
-          onLoad={map => setMyMap(map)}
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+
+
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 43.8563,
+            lng: -79.5085
+          }
+        }
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Avani Asian Indian Bistro'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
         >
-        </GoogleMap>
-  )
-
-  return isLoaded ? renderMap() : null;
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
 
-export default Map
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCTOD1Und52oU9unc9TRWlR0UOXqCEyFc8'
+})(MapContainer);
