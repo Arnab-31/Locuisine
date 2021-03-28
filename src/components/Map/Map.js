592298580ad1f7d705ddc3725f0f3e473fd91573
Krish Chopra <krish.chopra23@gmail.com>
2021-03-27 20:31:47 -0400

    updated map and local vendors list

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,7 +4,7 @@ import Marker from './Marker';
 
 const Map = (props: any) => {
     const [center, setCenter] = useState({lat: 43.7315, lng: -79.7624 });
-    const [zoom, setZoom] = useState(11);
+    const [zoom, setZoom] = useState(12);
     return (
         <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
@@ -13,10 +13,22 @@ const Map = (props: any) => {
           defaultZoom={zoom}
         >
           <Marker
-            lat={43.7315}
-            lng={-79.7624}
+            lat={43.70245044270797}
+            lng={-79.74904281494608}
             name="Your Location"
             color="blue"
+          />
+          <Marker
+            lat={43.726854}
+            lng={-79.815719}
+            name="The Spice Room"
+            color="red"
+          />
+          <Marker
+            lat={43.665983088504944}
+            lng={-79.73179229046802}
+            name="Silver Spoon Dinery"
+            color="red"
           />
           <Marker
             lat={43.757407301897615}
@@ -25,15 +37,21 @@ const Map = (props: any) => {
             color="red"
           />
           <Marker
-            lat={43.77186833422961}
-            lng={-79.6219686}
+            lat={43.727003917770034}
+            lng={-79.66631743493089}
             name="King Tandoori Grill"
             color="red"
           />
           <Marker
-            lat={43.60668832083854}
-            lng={-79.69193770477634}
+            lat={43.638681017140115}
+            lng={-79.72846899674875}
             name="Avani Asian Indian Bistro"
+            color="red"
+          />
+          <Marker
+            lat={43.68190769481575}
+            lng={-79.80320250483541}
+            name="Rashmi's Bakery"
             color="red"
           />
         </GoogleMapReact>
